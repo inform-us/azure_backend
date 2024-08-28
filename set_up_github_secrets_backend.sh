5b31a2aa62afd1ed169e60982ee70afeeb7d12c9
ruaridhg <ruaridhg@users.noreply.github.com>
2024-08-28 15:08:05 +0100

    Remove gh secrets before adding latest publish profile secret

--- set_up_github_secrets_backend.sh
+++ set_up_github_secrets_backend.sh
@@ -2,6 +2,11 @@
 
 GITHUB_TOKEN=$1
 
+# Loop through each secret and remove it
+for secret in $(gh secret list --json name --jq '.[] | .name'); do
+  gh secret remove $secret
+done
+
 # gh auth login
 gh auth login --with-token $GITHUB_TOKEN
 gh secret set AZUREAPPSERVICE_PUBLISHPROFILE_BACKEND --body "$(cat publishProfile.xml)"
