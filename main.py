e67d507880b50153ea3bb0c1bcc978de42493aca
ruaridhg <ruaridhg@users.noreply.github.com>
2024-08-29 09:00:29 +0100

    Changed welcome message to test az webapp sync command

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "Hello New World"}
+    return {"message": "Hello World"}
 
 @app.get("/api/data")
 async def get_data():
